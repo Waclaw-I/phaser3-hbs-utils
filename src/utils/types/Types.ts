aba503413ceebf3b693a70dea9fc7bb9ad77c062
Piotr 'Waclaw I' Hanusiak <hanusiakp@gmail.com>
2021-05-27 15:31:15 +0200

    version bump to 0.2.0

--- src/utils/types/Types.ts
+++ src/utils/types/Types.ts
@@ -8,19 +8,6 @@ export interface KeyFrame {
     frame?: string | number;
 }
 
-
-// TODO: Replace with types from phaser typedefs
-export interface TextConfig {
-    font: string;
-    text: string;
-    size: number;
-    color?: number;
-    origin?: {
-        x: number;
-        y?: number;
-    }
-}
-
 export interface Point {
     x: number;
     y: number;
